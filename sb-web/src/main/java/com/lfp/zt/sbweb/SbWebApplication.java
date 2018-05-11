01682b6582c9f18bc5b36727ad657ac871ec767a
zhutao <zhutao@qudian.com>
2018-05-11 17:10:02 +0800

    2018-05-11-feature-zhutao

---
+++ sb-web/src/main/java/com/lfp/zt/sbweb/SbWebApplication.java
@@ -0,0 +1,12 @@
+package com.lfp.zt.sbweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbWebApplication.class, args);
+    }
+}
